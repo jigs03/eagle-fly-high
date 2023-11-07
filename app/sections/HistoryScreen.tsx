e11bc32a93acaff24bc9ec35e72ea7cbc27551da
jigs03 <146357025+jigs03@users.noreply.github.com>
2023-11-08 01:59:20 +0800

    Update HistoryScreen.tsx

--- app/sections/HistoryScreen.tsx
+++ app/sections/HistoryScreen.tsx
@@ -40,19 +40,6 @@ const History = () => {
                 kejayaan demi kejayaan yang sudah dicapai. Antara yang tertinggi adalah menjadi
                 johan bagi pertadingan bola sepak antara kompeni 2 tahun berturut-turut pada tahun 2018.
             </p>
-
-            {images.map((batch, index) => (
-            <div key={index} className="flex items-center justify-center rounded-lg drop-shadow-lg border-2 border-solid border-orange-900 relative">
-                <Image
-                src={batch.src}
-                alt=""
-                fill
-                className="object-cover rounded-lg"
-                placeholder="blur"
-                loading="lazy"
-            />
-            </div>
-        ))}
         </div>
         </section>
     <Image src={LPH} alt="" className="w-screen rotate-180" />
