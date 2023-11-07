1453e65f347d61745f5c527db491e9705631eefa
jigs03 <146357025+jigs03@users.noreply.github.com>
2023-11-08 02:08:12 +0800

    Update HistoryScreen.tsx

--- app/sections/HistoryScreen.tsx
+++ app/sections/HistoryScreen.tsx
@@ -33,6 +33,7 @@ const History = () => {
         <h1 className="text-slate-100 drop-shadow-lg text-5xl mb-5">
         LATAR BELAKANG
         </h1>
+        <div>
         <RevealList
             reset={true}
             easing="ease-in-out"
@@ -40,15 +41,14 @@ const History = () => {
             interval={100}
             className="flex flex-col justify-center text-center text orange-100 tracking-wider invisible"
             >
-        <div>
             <p className="text-orange-300 text-center">
                 Kompeni Hang Nadim ditubuhkan pada 2017 hasil pecahan daripada batalion Jebat.
                 Pada tahun ini kompeni Hang Nadim sudah 5 tahun bertapak di ALK dan banyak
                 kejayaan demi kejayaan yang sudah dicapai. Antara yang tertinggi adalah menjadi
                 johan bagi pertadingan bola sepak antara kompeni 2 tahun berturut-turut pada tahun 2018.
             </p>
-        </div>
         </RevealList>
+        </div>
         </section>
     <Image src={LPH} alt="" className="w-screen rotate-180" />
     </>
