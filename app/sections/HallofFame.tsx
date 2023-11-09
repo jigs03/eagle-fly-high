7021aa7d9db0e93497ff85627dfbfe44025953bd
jigs03 <146357025+jigs03@users.noreply.github.com>
2023-11-09 10:59:30 +0800

    Update HallofFame.tsx

--- app/sections/HallofFame.tsx
+++ app/sections/HallofFame.tsx
@@ -45,7 +45,7 @@ const HallofFame = () => {
                         alt=""
                         className="object-fit relative rounded-lg"
                     />
-                    <div className="absolute bottom-5 bg-slate-100 py-1 px-2 rounded-lg flex items-center justify-center drop-shadow-lg ">
+                    <div className="absolute top-5 bg-slate-100 py-1 px-2 rounded-lg flex items-center justify-center drop-shadow-lg ">
                         <p className="text-3xl text-orange-300">{pictures.text}</p>
                     </div>
                 </div>
