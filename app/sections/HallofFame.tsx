5eb0b8610208795c1c9732b6aa564289361c50e8
jigs03 <146357025+jigs03@users.noreply.github.com>
2023-11-09 10:26:36 +0800

    Update HallofFame.tsx

--- app/sections/HallofFame.tsx
+++ app/sections/HallofFame.tsx
@@ -31,6 +31,19 @@ const HallofFame = () => {
                     key={index}
                     className="flex items-center justify-center rounded-lg drop-shadow-lg border-2 border-solid border-orange-900 relative"
                 >
+<<<<<<< HEAD
+=======
+                    <Image
+                        src = {pictures.src}
+                        alt = ""
+                        className = "object-fit relative rounded-lg"
+                    />
+                    <div className="absolute bottom-5 bg-slate-100 py-1 px-2 rounded-lg flex items-center justify-center drop-shadow-lg ">
+                        <p className="text-3xl text-orange-600">{pictures.text}</p>
+                    </div>
+                </div>
+                ))}
+>>>>>>> parent of 3bcc901 (Update HallofFame.tsx)
         </section>
         <Image src={LPH} alt="" className="w-screen rotate-180" />
         </>
